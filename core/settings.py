45bb36cdd768ee4bb98857b78b13e0ef43387a62
Pradip Kanawade <your-email@example.com>
2025-08-27 17:59:53 +0530

    Updated project code

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9ro*bh=oc0)97joj3mbb7w6u6irai2-_s^q09cp261^f(#+_@^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
